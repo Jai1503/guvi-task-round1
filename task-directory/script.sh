97d86c356836b7166e839f69ec59455044d9d135
Jai1503 <jayaprakash.vijayakumar15@gmail.com>
2023-01-22 05:24:24 +0000

    new commit

--- task-directory/script.sh
+++ task-directory/script.sh
@@ -4,13 +4,10 @@ do
 done
 
 git branch develop
-git checkout develop
-git add .
-git commit -m "develop commit"
-git checkout master
 
 read var1
 read var2
+
 if [ $var1=$var2 ]
 then
         tar -cvf files.tar /home/vagrant/task-directory
@@ -18,5 +15,8 @@ then
         git commit -m "commit to master"
         git push origin master
 else
+	git checkout develop
+	git add .
+	git commit -m "commit to develop"
         git push origin develop
 fi
